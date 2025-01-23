0714f9110cc9ce99928612602360da157e0781bf
Wada Gift <wgift41@gmail.com>
2025-01-23 23:51:12 +0100

    more workingd

--- Components/Home/Home.tsx
+++ Components/Home/Home.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import TypeOfCam from './TypesOfCam/TypesOfCam'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
 // import Destination from './Destination/Destination'
 
 const Home = () => {
+    useEffect(() => {
+        const initAOS = async () => {
+            await import('aos');
+            AOS.init({
+                duration: 1000,
+                easing: 'ease',
+                once: true,
+                anchorPlacement: 'top-bottom'
+            })
+        }
+        initAOS()
+    }, []);
     return (
         <div className='overflow-hidden '>
            <Hero/>
