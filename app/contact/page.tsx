c97b31d4288d783a40a892fa20e29885317b8a6c
Wada Gift <91764732+OgeeTech@users.noreply.github.com>
2025-06-29 19:32:42 +0100

    Added page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from '@/Components/Home/Contact/Contact';
+
+const ContactPage = () => {
+  return (
+    <div className='pt-[12vh]'>
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactPage;
